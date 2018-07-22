bc643fc40f84cbd650dd4ca6e04225275dedfeae
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2018-07-22 20:52:36 +0100

    cahnged delaration and initalisation to include sub struct/ nested struct

--- cmd/src/main.go
+++ cmd/src/main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email    string
+	postCode string
+}
 type person struct {
 	fName string
 	lName string
+	info  contactInfo
 }
 
 func main() {
@@ -12,17 +17,17 @@ func main() {
 	//three ways of declaring and initalising struct
 
 	//1.
-	sebastian := person{"Sebastian", "Flynn"}
+	sebastian := person{"Sebastian", "Flynn", contactInfo{"seb@someplace.com", "ls9 2ho"}}
 
 	//2.
-	erin := person{fName: "Erin", lName: "Flynn"}
+	erin := person{fName: "Erin", lName: "Flynn", info: contactInfo{email: "erin@somplace.com", postCode: "ls9 2ho"}}
 
 	//3.
 	var toby person
 
-	toby.fName, toby.lName = "Toby", "Flynn"
+	toby.fName, toby.lName, toby.info.email, toby.info.postCode = "Toby", "Flynn", "toby@someplace.com", "ls9 2ho"
 
 	//same print out for all
 
-	fmt.Println(sebastian, erin, toby)
+	fmt.Println("\n", sebastian, "\n", erin, "\n", toby)
 }
